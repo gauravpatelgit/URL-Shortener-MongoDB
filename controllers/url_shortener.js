dba84202d34acaf05264df5673caf0fd111ec469
Gaurav Patel <gauravpateldws@gmail.com>
2026-05-06 13:15:53 +0530

    fixed backend issue

--- controllers/url_shortener.js
+++ controllers/url_shortener.js
@@ -60,7 +60,7 @@ const createShortUrl = async (req, res) => {
     console.error("🔥 ERROR:", err);
     res.status(500).json({
       success: false,
-      error: err,
+      error: err.message,
       message: "Server error ❌",
     });
   }
